b3a7ee8d6c155e418bb51e054459de1a56d4707f
Holger Thies <info@holgerthies.com>
2021-05-19 13:00:56 +0900

    added experimental data

---
+++ src/powerseries_double.h
@@ -0,0 +1,291 @@
+#ifndef POWERSERIES_H
+#define POWERSERIES_H
+#include "polynomial.h"
+#include <numeric>
+namespace iRRAM{
+
+  
+
+  template<unsigned int d, class T>
+  class Powerseries {
+  private:
+    std::function<T(const Multiindex<d>&)> coeff_fun;
+    mutable Polynomial<d,T> trunc_series;
+    double get_factor(int n, int m) const{
+      return FactorCache::get_factor(n,m);
+    }
+    double get_factor(const std::array<unsigned int,d>& n,const std::array<unsigned int,d>& m) const{
+      REAL ans=1;
+      for(int i=0; i<d; i++)
+        ans *= FactorCache::get_factor(n[i], m[i]);
+      return ans;
+    }
+    double get_bound_complex(const int deg, const std::array<T,d>& x) const{
+      REAL r = get_radius();
+      REAL dist = r-inf_norm(x);
+      if(dist < 0){
+        cout << "Warning: radius too small" << std::endl;
+      }
+      return M*power(r, d)/power(dist,deg+d);
+    }
+
+    void extend_trunc_series() const{
+      for(auto index : max_degree_indices<d>(trunc_series.get_degree()+1)){
+        trunc_series.set_coefficient(index,coeff_fun(index));
+      }     
+    }
+
+  protected:
+    vector<T,d> center;
+    REAL radius,M;
+  public:
+    Powerseries<d,T>(const Powerseries<d,T>& ps) : center(ps.get_center()), radius(ps.get_radius()), M(ps.get_M()){
+      coeff_fun = [&ps] (const Multiindex<d>& index) {return ps.get_coefficient(index);};
+    }
+
+    Powerseries<d,T>(const std::function<T(const Multiindex<d>&)>& coeff_fun, const vector<T,d>& center, const REAL& radius, const REAL& M) :  coeff_fun(coeff_fun), center(center), radius(radius), M(M) {
+    }
+
+    Powerseries<d,T>() : Powerseries([] (const Multiindex<d>&){return 0;}, vector<T,d>(), 1, 0) {};
+
+    T get_coefficient(const Multiindex<d>& index) const{
+      while(trunc_series.get_degree() <= max(index)){
+        extend_trunc_series();
+      }
+      return trunc_series.get_derivative(index);
+    }
+
+    vector<T,d> get_center() const {return center;}
+
+    REAL get_radius() const {return radius;}
+
+    REAL get_M() const {return M;}
+
+    // T evaluate(const std::array<unsigned int,d>& index, const std::array<T,d>& x) const override{
+    //   return sum(index,x);
+    // }
+
+    T partial_sum(const vector<T,d>& x, const int degree) const {
+      while(trunc_series.get_degree() <= degree){
+        extend_trunc_series();
+      }
+      return trunc_series(x);
+    }
+
+    int get_cache_size() const{
+      return trunc_series.get_degree();
+    }
+
+    Polynomial<d,T> get_truncated_series(const int degree) const{
+      while(trunc_series.get_degree() <= degree){
+        extend_trunc_series();
+      }
+      return trunc_series;
+    }
+
+    T sum(const vector<T,d>& x) const {
+      vector<T, d> xc = x - center;
+      auto xmax = inf_norm(xc);
+      double error_factor = power(xmax/radius,d);
+      double error = M*power(1-xmax/radius, d);
+      Multiindex<d> zero{};
+      double sum = get_coefficient(zero);
+      double ans = sum;
+      int deg=0;
+      while (error > 0.000001){
+        deg++;
+        for(auto idx : max_degree_indices<d>(deg)){
+          sum += get_coefficient(idx)*power(xc, idx);
+        }
+        error *= error_factor;
+      }
+     return ans;
+    } 
+    // T sum(const std::array<unsigned int,d>& index, const std::array<T,d>& x) const {
+    //   std::array<T, d> xc;
+    //   std::transform(x.begin(), x.end(), center.begin(),xc.begin(), std::minus<T>());
+      
+    //   T sum=get_factor(index,index)*get_coefficient(index);
+    //   T ans = sum;
+    //   REAL error = 0;
+    //   for(auto p : partitions<d>(1)){
+    //     std::array<unsigned int,d> p2;
+    //     std::transform(index.begin(), index.end(), p.begin(),p2.begin(), std::plus<unsigned int>());
+    //     REAL bnd = minimum(get_bound(p2), get_bound_complex(p2, x));
+    //     error += bnd*power<d,T>(xc,p2);
+    //   }
+    //   int i = 0;
+    //   sizetype trunc_error = real_to_error(error), sum_error,total_error;
+    //   sum.geterror(sum_error);
+    //   sizetype_add(total_error, sum_error, trunc_error);
+    //   while (sizetype_less(sum_error, trunc_error) &&
+    //          (trunc_error.exponent >= ACTUAL_STACK.actual_prec) ){
+    //     i++;
+    //     for(auto p : partitions<d>(i)){
+    //       std::array<unsigned int,d> p2;
+    //       std::transform(index.begin(), index.end(), p.begin(),p2.begin(), std::plus<unsigned int>());
+    //       sum += get_factor(p2,index)*get_coefficient(p2)*power<d,T>(xc, p2);
+    //     }
+    //     error = 0;
+    //     for(auto p : partitions<d>(i+1)){
+    //       std::array<unsigned int,d> p2;
+    //       std::transform(index.begin(), index.end(), p.begin(),p2.begin(), std::plus<unsigned int>());
+    //       REAL bnd = minimum(inv_factorial<d>(p)*get_bound(p2), get_bound_complex(p2, x));
+    //       error += bnd*power<d,T>(xc,p2);
+    //     }
+    //     trunc_error = real_to_error(error);
+    //     sum.geterror(sum_error);
+    //     sizetype curr_error;
+    //     sizetype_add(curr_error, sum_error, trunc_error);
+    //     if(sizetype_less(curr_error, total_error)){
+    //       ans = sum;
+    //       ans.seterror(curr_error);
+    //       total_error = curr_error;
+    //     }
+    //   }
+    //   return ans;
+    // }
+
+    // Powerseries<d,T>& operator+=(const Powerseries<d,T>& f){
+    //   *this = *this + f;
+    //   return *this;
+    // }
+
+  };
+  template<unsigned int d, class T>
+  class PS_ptr {
+  private :
+    std::shared_ptr<Powerseries<d,T>> ps;
+  public:  
+    PS_ptr() : ps(std::make_shared<Powerseries<d,T>>()) {};
+    PS_ptr(const std::shared_ptr<Powerseries<d,T>>& ps) : ps(ps) {};
+    Powerseries<d,T>* operator->() const{
+      return ps.get();
+    }
+    T operator()(const std::array<T,d>& x) const {
+      return (*ps)(x);
+    }
+    std::shared_ptr<Cinfinity<d,T>> fun() const{
+      return ps;
+    }
+    T operator()(const vector<T,d>& x){
+      return ps->sum(x);
+    }
+    PS_ptr<d,T>& operator+=(const PS_ptr<d,T>& p2){
+      auto sum = *this + p2;
+      *this = sum;
+      return *this;
+    }
+  };
+
+  template<unsigned int d, class T>
+  PS_ptr<d,T> to_powerseries(const CinfinityPtr<d,T>& f, const vector<T,d>& center, const REAL& r, const REAL& M){
+    auto g = f->deep_copy();
+    g->set_center(center);
+    return PS_ptr<d,T>(std::make_shared<Powerseries<d,T>>([g] (const Multiindex<d>& index) {return g->get_derivative(index);}, center, r, M));
+  }
+
+// template <unsigned int d, unsigned int m, unsigned int n, class T>
+// class MVPowerseries : public Matrix<m,n,PS_ptr<d,T>> {
+// private:
+// public:
+//   using Matrix<m,n,PS_ptr<d,T>>::Matrix;
+//   MVPowerseries(const Matrix<m,n,PS_ptr<d,T>>& M) : Matrix<m,n,PS_ptr<d,T>>(M) {};
+//   using Matrix<m,n,PS_ptr<d,T>>::operator();
+
+//   MVPowerseries<d,m,n,T> operator-();
+//   MVPowerseries<d,m,n,T> partial_derivative(const unsigned int i) const;
+//   Matrix<m,n,T> operator()(const std::array<T,d>& x) const{
+//     Matrix<m,n,T> ans;
+//     for(unsigned int i=0; i < m; i++){
+//       for(unsigned int j=0; j < n; j++){
+//         ans(i,j) = (*this)(i,j)(x);
+//       }
+//     }
+//     return ans;
+//   }
+
+//   auto get_center() const{
+//     return (*this)(0,0)->get_center();
+//   }
+
+//   REAL get_radius() const{
+//     REAL ans = (*this)(0,0)->get_radius();
+//     for(int i=0; i<m;i++){
+//       for(int j=0; j<n; j++){
+//         ans = minimum(ans, (*this)(i,j)->get_radius());
+//       }
+//     }
+//     return ans;
+//   }
+
+//   REAL get_bound(const std::array<unsigned int,d>& index) const{
+//     REAL ans = (*this)(0,0)->get_bound(index);
+//     for(int i=0; i<m;i++){
+//       for(int j=0; j<n; j++){
+//         ans = maximum(ans, (*this)(i,j)->get_bound(index));
+//       }
+//     }
+//     return ans;
+//   }
+
+// };
+// // compute partial derivative
+//   template <unsigned int d, unsigned int m, unsigned int n, class T>
+//   MVPowerseries<d,m,n,T> MVPowerseries<d,m,n,T>::partial_derivative(const unsigned int i) const{
+//     std::array<unsigned int, d> index{};
+//     index[i-1] = 1;
+//     MVPowerseries<d,m,n,T> ans;
+//     for(unsigned int i=0; i < m; i++){
+//       for(unsigned int j=0; j < n; j++){
+//         ans(i,j) = std::make_shared<Powerseries<d,REAL>>(std::make_shared<Cinfinity<d,REAL>>((*this)(i,j)->derive(index)), (*this)(i,j)->get_center(), (*this)(i,j)->get_radius(), (*this)(i,j)->get_M());
+//       }
+//     }
+//     return ans;
+//   }
+
+// // redefine overloaded operators on matrix type
+// template <unsigned int d, unsigned int m, unsigned int n, class T>
+// MVPowerseries<d,m,n,T> operator+(const MVPowerseries<d,m,n,T>& lhs, const MVPowerseries<d,m,n,T>& rhs){
+//   return add(lhs,rhs);
+// }
+
+// template <unsigned int d, unsigned int m, unsigned int n, class T>
+// MVPowerseries<d,m,n,T> operator-(const MVPowerseries<d,m,n,T>& lhs, const MVPowerseries<d,m,n,T>& rhs){
+//   return subtract(lhs,rhs);
+// }
+
+// template <unsigned int d, unsigned int m, unsigned int n, unsigned int k, class T>
+// MVPowerseries<d,m,k,T> operator*(const MVPowerseries<d,m,n,T>& lhs, const MVPowerseries<d,n,k,T>& rhs){
+//   return multiply(lhs,rhs);
+// }
+
+// template <unsigned int d, unsigned int m, unsigned int n, class T>
+// MVPowerseries<d,m,n,T> operator*(const T& lhs, const MVPowerseries<d,m,n,T>& rhs){
+//   MVPowerseries<d,m,n,T> ans;
+//   for(int i=0; i<m;i++){
+//     for(int j=0;j<n;j++){
+//       ans(i,j) = lhs*rhs(i,j);
+//     }
+//   }
+//   return ans;
+// }
+// template <unsigned int d, unsigned int m, unsigned int n, class T>
+// MVPowerseries<d,m,n,T> operator*(const MVPowerseries<d,m,n,T>& lhs, const T& rhs){
+//   return multiply(lhs,rhs);
+// }
+
+// template<unsigned int d, unsigned int m, unsigned int n,class T>
+// MVPowerseries<d,m,n,T> toPowerseries(const MVFunction<d,m,n,T>& f, const std::array<T,d> center, const REAL& radius, const REAL& M){
+//   MVPowerseries<d,m,n,T> ans;
+//   for(int i=0; i<m;i++){
+//     for(int j=0;j<n;j++){
+//       ans(i,j) = std::make_shared<Powerseries<d,T>>(f(i,j), center, radius,M);
+//     }
+//   }
+//   return ans;
+// }
+}
+
+
+#endif
---
+++ test/ck_double.cc
@@ -0,0 +1,188 @@
+using REAL = double;
+using INTEGER = long;
+#include <iostream>
+using namespace std;
+namespace iRRAM{
+double power(const double& a, const int& b) {return pow(a,b);}
+}
+double maximum(const double& a, const double& b) { return max(a,b); }
+#include "combinatorics.h"
+#include "cinfinity.h"
+#include "operator.h"
+#include "funops.h"
+// #include "matrix.h"
+#include "powerseries_double.h"
+#include "mfun.h"
+#include "polynomial.h"
+#include "diffop.h"
+#include "pde.h"
+#include "pde_examples.h"
+using namespace iRRAM;
+template<unsigned int m, unsigned int n>
+void print(const Matrix<m,n,double>& M){
+  for(int i=0; i<m; i++){
+    for(int j=0;j<n;j++){
+      std::cout << M(i,j) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(){
+  FactorCache::init();  
+  // REALMATRIX A = zeroes(2,2), B=zeroes(2,2);
+  // A(0,0) = 6;
+  // A(1,1) = 1/A(0,0);
+  // B(0,1) = 2;
+  // B(1,0) = B(0,1);
+
+  // cout << to_string_double(symm_eig(A)) << std::endl;
+  // cout << to_string_double(symm_eig(B)) << std::endl;
+  // auto s = pencil_decomposition(A, B, 2, 2);
+  // cout << to_string_double(s.A.eigenvalues) << std::endl;
+  // cout << to_string_double(s.B.eigenvalues) << std::endl;
+  // cout << to_string_double(s.B.eigenvectors) << std::endl;
+
+
+   auto acoustics = acoustics_2d<double>(1,2);
+
+  auto D_le = linear_elasticity(3,2,5);
+   int dimension=0, system;
+   struct rusage usage;
+   struct timeval start, end;
+   auto f = sine<3>(1,5);
+   auto f2 = sine<2>(1,REAL(0.5));
+   auto g = sine<3>(2,3);
+   auto g2 = sine<2>(0,1);
+   auto h = sine<3>(0,REAL(0.5));
+   auto v = R2M<3,9,1,REAL>({1,2,3,4,5,6,7,8,9});
+   v(0,0) = f;
+   v(3,0) = f*g;
+   v(4,0) = h+g+f;
+   auto v2 = R2M<2,3,1,REAL>({1,2,3});
+   v2(0,0) = f2;
+   v2(1,0) = f2+g2;
+   auto elasticity = linear_elasticity(1,2,3);
+   auto elasticity_const = linear_elasticity_const(1,2,3);
+   auto acoustics_const = acoustics_2d_const(1,2);
+  // auto v2 = v;
+  // auto Di = system1c;
+  // for(int i=0; i<100; i++){
+  //   cout << "i:" << i << std::endl;
+  //   //v2 = system1c(v2);
+  //   //print(v2.get_derivative({0,0,0}));
+  //   cout << std::endl;
+  //   cout <<"Equals:"<< std::endl;
+  //   cout << std::endl;
+  //   print(Di(v).get_derivative({0,0,0}));
+  //   Di = system1c*Di;
+  // }
+
+
+   auto system2 = linear_elasticity_trig();
+   cout << "choose example" << std::endl;
+   cout << "1) 2D acoustics with constant coefficients" << std::endl;
+   cout << "2) 2D acoustics with constant function coefficients" << std::endl;
+   cout << "3) 3D linear elasticitiy with constant coefficients" << std::endl;
+   cout << "4) 3D linear elasticitiy with constant coefficients (method 2)" << std::endl;
+   cout << "5) 3D linear elasticitiy with constant function coefficients" << std::endl;
+   cout << "6) 3D linear elasticitiy with trigonometric coefficients" << std::endl;
+   cin >>  system;
+  
+   iRRAM::vector<REAL, 3> x;
+   iRRAM::vector<REAL, 2> x2;
+   REAL r_max;
+   switch(system){
+     case 1: 
+      dimension = 2;
+      r_max = get_sol_r_const(2, 3, 1, 1);
+     break;
+     case 2: 
+      dimension = 2;
+      r_max = get_sol_r(2, 3, 1, 1);
+     break;
+     case 3: 
+      dimension = 3;
+      r_max = get_sol_r_const(3, 9, 1, 1);
+     break;
+     case 4: 
+      dimension = 3;
+      r_max = get_sol_r_const(3, 9, 1, 1);
+     break;
+     case 5: 
+      dimension = 3;
+      r_max = get_sol_r(3, 9, 1, 1);
+     break;
+     case 6: 
+      dimension = 3;
+      r_max = get_sol_r(3, 9, 1, 1);
+     break;
+   }
+   cout << "choose x "  << std::endl;
+   for(int i=0; i<dimension; i++){
+     cin >> x[i];
+   }
+   for(int i=0; i<2; i++){
+      x2[i] = x[i];
+   }
+   REAL t;
+   cout << "choose t < " << r_max << std::endl;
+   cin >>  t;
+
+  
+   getrusage(RUSAGE_SELF, &usage);
+   start = usage.ru_utime;
+  
+   getrusage(RUSAGE_SELF, &usage);
+   start = usage.ru_utime;
+   std::vector<REAL> ys;
+   
+   if(system == 1){
+     auto sol = solve_pde_constant(acoustics_const, v2, x2, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   if(system == 2){
+     auto sol = solve_pde(acoustics, v2, x2, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   if(system == 3){
+     auto sol = solve_pde_constant(elasticity_const, v, x, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   if(system == 4){
+     auto sol = solve_pde_constant_method2(elasticity_const, v, x, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   if(system == 5){
+     auto sol = solve_pde(elasticity, v, x, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   if(system == 6){
+     auto sol = solve_pde(system2, v, x, 1, 1);
+     for(auto& s : sol){
+       ys.push_back(s.sum({t}));
+     }
+   }
+   cout << std::endl;
+   getrusage(RUSAGE_SELF, &usage);
+   end = usage.ru_utime;
+   auto iteration_time =  end.tv_sec-start.tv_sec+double(end.tv_usec-start.tv_usec)/1000000;
+   for(auto& c : ys){
+     std::cout << c << std::endl; 
+   }
+   std::cout << " dimension:" << dimension;
+   std::cout << " system:" << system;
+   std::cout << " time:" << iteration_time;
+   std::cout << std::endl;
+  
+}
